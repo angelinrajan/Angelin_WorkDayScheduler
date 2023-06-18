c1e811018e3bf03cf50fe16ecde253d024c14200
AR <angelinrajan20@gmail.com>
2023-06-18 10:47:07 -0400

    adding codes to js

--- Develop/script.js
+++ Develop/script.js
@@ -1,18 +1,57 @@
-
-
+storage = window.localStorage;
+var timeSlot = $('.text-field');
+var description = $('.description').val();
 const today = dayjs();
 var rowPast = $('.past')
 var saveButton = $('.saveBtn')
+var fullSchedule = $('.daySchedule');
+var nowTime = dayjs().format("hA");
+console.log(nowTime);
+var hour = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"];
 
-var day = today.format('dddd, MMMM D');
-$('#currentDay').text(day)
+var indexRow = $('.time-block');
+
+for (i=0; i<hour.length; i++) {
+  var time= $('.textfield').val(hour[i]);
+  if (hour[i] > nowTime) {
+    indexRow.addClass("future");
+  } else if (hour[i] === nowTime) {
+    indexRow.addClass("present");
+  } else {
+    indexRow.addClass("past");
+  }
+  console.log(indexRow);
+}
+
+
+var gettexts = storage.getItem('value');
+var text;
+if (gettexts === null) {
+text = [];
+} else {
+  text = JSON.parse(gettexts)
+  console.log(text);
+};
+
+// Attach click event listener to each save button
+$(".time-block").on("click", ".saveBtn", function() { 
+
+  var eventText = $(this).siblings(".description").val();
+  var eventTime = $(this).siblings(".hour").text();
+//var neweventText = JSON.stringify(eventText);
+  storage.setItem(eventTime, eventText);
+console.log(eventTime +" : " + eventText);
+
+});
+
+
+
 //currentDate = dayjs()
 //console.log(currentDate);
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
-  //rowPast.on('click', handleSavePast);
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -31,4 +70,26 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
-});
+  function displayDate() {
+    var day = today.format('dddd, MMMM D');
+    $('#currentDay').text(day)
+    }
+    displayDate();
+
+
+//var btnClicked = $(event.target);
+//btnClicked.parent('.description').save();
+//saveButton.on('click',saveClicked);
+//timeSlots();
+/*
+for (i=0; i<hours.length; i++) {
+  if (nowTime< 9) {
+    console.log("time in past")
+  } else if (nowTime== hours[i]) {
+    console.log("currenttime")
+  } else {
+    console.log("future time")
+  };
+}
+}timeSlots();
+*/
